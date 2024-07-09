527354aa127a30a7e81bbd1b1cf3352555c940fe
TamerlanMustafayev <tamerlan.m.2000@gmail.com>
2024-07-09 00:02:39 -0400

    Exercise 4. I had 2 apporaches for this one. Method 1 is commented out.

--- app.js
+++ app.js
@@ -38,3 +38,28 @@ Complete Exercise 3 in the space below:
 foods.unshift('taco')
 
 console.log('Exercise 3 result:', foods);
+
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+// METHOD 1 for Exercise 4
+// console.log(foods.indexOf('pizza')) // gives me the index of 'pizza' which is 1
+
+// const favFood = foods[1] // then I used 1 to assign 'pizza' to a new variable
+
+// METHOD 2 for Exercise 4
+
+const favFood = foods[foods.indexOf('pizza')]
+
+
+
+console.log('Exercise 4 result:', favFood);
