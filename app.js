7484a445815b9a34c9f03c579fc8742834b184e9
TamerlanMustafayev <tamerlan.m.2000@gmail.com>
2024-07-10 19:07:30 -0400

    Exercise 12. I used for of method here but i am not fully satisfied with it. I will try another one too."

--- app.js
+++ app.js
@@ -189,6 +189,43 @@ console.log('Exercise 11 result:', odds);
 
 
 
+/*
+Exercise 12: FizzBuzz with arrays
+
+1) Choose a method to iterate through the `nums` array. 
+
+2. As you loop, sort the numbers into new arrays based on the following rules:
+
+   - Push any number evenly divisible by 3 to an array called `fizz`.
+   - Push any number evenly divisible by 5 to an array called `buzz`.
+   - Push any number that is evenly divisible by 3 and 5 to an array called
+     `fizzbuzz`.
+
+   Note: A single number may meet more than one of the above rules. If it does,
+         it should be placed in multiple arrays. For example, the number `15`
+         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+
+Complete Exercise 12 in the space below:
+*/
+
+const fizz = []
+const buzz = []
+const fizzbuzz = []
+
+for (const num of nums) {
+   num%3===0 ? fizz.push(num) : console.log('not divisible by 3') /// i dont know if using ternary methods are recommended. Also i dont acutally want to console.log something here. i dont know how to ask js ton NOT to anything if the conditional is false
+   num%5===0 ? buzz.push(num) : console.log('not divisible by 5') ///
+   num%5===0&&num%3===0 ? fizzbuzz.push(num) : console.log('idek') /// 
+}
+
+
+
+
+
+console.log('Exercise 12 Results:');
+console.log('  fizz:', fizz);
+console.log('  buzz:', buzz);
+console.log('  fizzbuzz:', fizzbuzz);
 
 
 
